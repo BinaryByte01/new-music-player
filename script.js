b738e3e7f7b7988a72d302f44e084a0a7a16863c
Sambhav Jindal <sambhav.jindal1303@gmail.com>
2024-03-18 16:59:08 +0530

    change in js file

--- script.js
+++ script.js
@@ -17,7 +17,7 @@ function convertSecondsToMinutesAndSeconds(seconds) {
 
 async function getSongs(folder) {
     currFolder = folder;
-    let a = await fetch(`http://127.0.0.1:5500/${folder}`)
+    let a = await fetch(`${folder}`)
     let response = await a.text()
     // console.log(response)
 
@@ -73,7 +73,7 @@ const playMusic = (track, pause = false) => {
 }
 
 async function displayAlbums() {
-    let a = await fetch(`http://127.0.0.1:5500/songs/`)
+    let a = await fetch(`songs/`)
     let response = await a.text()
     let div = document.createElement("div");
     div.innerHTML = response;
@@ -87,7 +87,7 @@ async function displayAlbums() {
         if (e.href.includes("/songs/")) {
             let folder = e.href.split("/").slice(-1)[0];
             // get the metadata of the folder
-            let a = await fetch(`http://127.0.0.1:5500/songs/${folder}/info.json`)
+            let a = await fetch(`songs/${folder}/info.json`)
             let response = await a.json()
             console.log(response);
 
